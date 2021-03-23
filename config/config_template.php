800ecb60e8de6e8f73d446d8bd7927bae3566c12
Ruben Christoffer Hegland-Antonsen <rubench@stud.ntnu.no>
2021-03-23 11:04:37 +0100

    Added config template

---
+++ config/config_template.php
@@ -0,0 +1,12 @@
+<?php
+
+// NOTE: This is a template file that should be copied and renamed 'config.php'
+// with the appropriate fields filled in.
+// This is to ensure that sensitive information is not present in the remote git repository.
+return (object) array(
+    'db_host' => '127.0.0.1',
+    'db_port' => '3306',
+    'db_username' => '',
+    'db_password' => '',
+    'db_name' => '',
+);
