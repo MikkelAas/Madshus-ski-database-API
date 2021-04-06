0bb35e25a37fbd29c1441de15998bc4f8bee6aab
Ruben Christoffer Hegland-Antonsen <rubench@stud.ntnu.no>
2021-04-06 14:52:17 +0200

    Made it possible to use different config files

--- config/config_template.php
+++ config/config_template.php
@@ -3,7 +3,8 @@
 // NOTE: This is a template file that should be copied and renamed 'config.php'
 // with the appropriate fields filled in.
 // This is to ensure that sensitive information is not present in the remote git repository.
-return (object) array(
+const config = array(
+    'db_driver' => 'mysql',
     'db_host' => '127.0.0.1',
     'db_port' => '3306',
     'db_username' => '',
--- db/db.php
+++ db/db.php
@@ -5,18 +5,16 @@ class DB {
     protected PDO $pdo;
 
     public function __construct () {
-        $config = require_once('config/config.php');
-
         $options = [
             PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
             PDO::ATTR_EMULATE_PREPARES   => true,
         ];
 
-        $dsn = sprintf("mysql:host=%s;dbname=%s;port=%s;charset=%s", $config->db_host, $config->db_name, $config->db_port, $config->db_charset);
+        $dsn = sprintf("%s:host=%s;dbname=%s;port=%s;charset=%s", config['db_driver'], config['db_host'], config['db_name'], config['db_port'], config['db_charset']);
 
         try {
-            $this->pdo = new PDO($dsn, $config->db_username, $config->db_password, $options);
+            $this->pdo = new PDO($dsn, config['db_username'], config['db_password'], $options);
         } catch (PDOException $e) {
             echo "Something went wrong connecting to database! Please contact system administrator if problem persists.";
 
