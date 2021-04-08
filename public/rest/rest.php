641b5696b43db9a2c80ad265809d8c16b64ac01a
Ruben Christoffer Hegland-Antonsen <rubench@stud.ntnu.no>
2021-04-08 17:12:32 +0200

    Added default REST handler

--- public/rest/rest.php
+++ public/rest/rest.php
@@ -8,7 +8,11 @@ require_once('../../config/config.php');
 
 $path = $_SERVER['REQUEST_URI'];
 
+header("content-type: application/json");
+
 if (array_key_exists($path, endpoints)) {
-    header("content-type: application/json");
     require(endpoints[$path]);
+} else {
+    http_response_code(404);
+    echo json_encode(array("error"=>"That endpoint does not exist!"));
 }
