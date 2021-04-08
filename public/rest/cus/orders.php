9a3c8a968be64589c79ce00b78f7ed17a6ac8baa
Mikkel Aas <aa.mikkel2902@gmail.com>
2021-04-08 19:05:01 +0200

    Implemented the orders endpoint in orders.php

--- public/rest/cus/orders.php
+++ public/rest/cus/orders.php
@@ -1,21 +1,27 @@
 <?php
 
-require ('../../../config/config.php');
 require ('models/OrderModel.php');
 
 $queries = array();
-parse_str($_SERVER['QUERY_STRING'], $queries);
-
-header("Content-Type: application/json; charset=UTF-8");
-
 $orderModel = new OrderModel();
-if (array_key_exists("since", $queries)){
-    $res = $orderModel->getOrders($queries['since']);
-} else {
-    $res = $orderModel->getOrders('');
-}
 
+switch ($method){
+    case "GET":
+        if (array_key_exists("since", $queries)){
+            $res = $orderModel->getOrders($queries['since']);
+        } else {
+            $res = $orderModel->getOrders('');
+        }
+        break;
+    default:
+        http_response_code(405);
+        echo json_encode(array("error"=>"invalid method " . $method));
+        return;
+}
 if (!empty($res)){
     echo json_encode($res);
+} else {
+    http_response_code(400);
+    echo json_encode(array("error"=>"no results"));
 }
 
