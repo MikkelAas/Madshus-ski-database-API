c2e375d9a4e8a8472520c155ba9d42e1fa3921c5
Mikkel Aas <aa.mikkel2902@gmail.com>
2021-04-19 12:05:26 +0200

    Added a test for getting a plan in PlanModelTest.php

--- tests/unit/PlanModelTest.php
+++ tests/unit/PlanModelTest.php
@@ -1,1 +1,18 @@
 <?php
+require_once('config/config_test.php');
+require('models/PlanModel.php');
+class PlanModelTest extends \Codeception\Test\Unit{
+
+    public function testCreatePlan(){
+
+    }
+
+    public function testGetPlan(){
+        $planModel = new PlanModel();
+
+        $res = $planModel->getPlan();
+
+        self::assertCount(1, $res);
+        self::assertStringContainsStringIgnoringCase("2021-04-08", $res[0]);
+    }
+}
