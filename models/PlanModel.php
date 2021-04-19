cc9bf59f212cfa9d6ac28f523dbdcfdcccd4def0
Mikkel Aas <aa.mikkel2902@gmail.com>
2021-04-19 12:24:32 +0200

    Made the getPlan function query for the most recent date in the database instead of todays date in PlanModel.php

--- models/PlanModel.php
+++ models/PlanModel.php
@@ -22,32 +22,39 @@ class PlanModel{
      * @return array Returns an array with the specified information.
      */
     public function getPlan():array{
-        $currentDate = date('Y-m-d');
+
+        $query1 = 'SELECT MAX(start_date) FROM production_plan';
+
+        $stmt = $this->db->prepare($query1);
+        $stmt->execute();
+        
+        $mostRecentDate = $stmt->fetchColumn(0);
+
         // Retrieves the start date, the ski type, and the amount for the current date.
-        $query = '
+        $query2 = '
             SELECT production_plan.start_date, production_plan_ski.ski_type_id, production_plan_ski.daily_amount
             FROM production_plan
             INNER JOIN production_plan_ski ON production_plan.id = production_plan_ski.production_plan_id
             WHERE production_plan.start_date = :date_now
         ';
 
-        $stmt = $this->db->prepare($query);
-        $stmt->bindValue(':date_now', $currentDate);
+        $stmt = $this->db->prepare($query2);
+        $stmt->bindValue(':date_now', $mostRecentDate);
         $stmt->execute();
 
         $res = array();
 
         while ($row = $stmt->fetch()){
-            $currentDate = $row['start_date'];
-            if (!array_key_exists($currentDate, $res)){
-                $res[$currentDate] = array(array(
-                    'start_date'=>$currentDate,
+            $mostRecentDate = $row['start_date'];
+            if (!array_key_exists($mostRecentDate, $res)){
+                $res[$mostRecentDate] = array(array(
+                    'start_date'=>$mostRecentDate,
                     'ski_type_id'=>$row['ski_type_id'],
                     'daily_amount'=>$row['daily_amount'],
                     array()
                 ));
             }
-            array_push($res[$currentDate][1], array('ski_type_id'=>$row['ski_type_id'], 'daily_amount'=>$row['daily_amount']));
+            array_push($res[$mostRecentDate][1], array('ski_type_id'=>$row['ski_type_id'], 'daily_amount'=>$row['daily_amount']));
         }
 
         return array_values($res);
