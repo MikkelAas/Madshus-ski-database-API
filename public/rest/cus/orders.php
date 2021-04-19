e7e007f6a353486c00513d6734da027660614603
Mikkel Aas <aa.mikkel2902@gmail.com>
2021-04-19 09:59:49 +0200

    Made the delete order endpoint and added some rest constant messages.

--- public/rest/cus/orders.php
+++ public/rest/cus/orders.php
@@ -13,6 +13,9 @@ switch ($method){
             $res = $orderModel->getOrders($queries["since"]);
         } else if (!array_key_exists(4, $pathParts)){
             $res = $orderModel->getOrders(NULL);
+        } else {
+            http_response_code(400);
+            echo json_encode(array("error"=>"invalid endpoint"));
         }
         break;
     case "POST":
@@ -29,12 +32,26 @@ switch ($method){
             $quantity = $data['quantity'];
 
             $orderModel->createOrder($totalPrice, $refToLargerOrder, $customerId, $stateMessage, $employeeId, $skiTypeId, $quantity);
+            http_response_code(201);
             return;
         } else {
             http_response_code(400);
             echo json_encode(array("error"=>"invalid endpoint"));
             return;
         }
+    case "DELETE":
+        if (array_key_exists(4, $pathParts) && is_numeric($pathParts[4])){
+            $data = json_decode(file_get_contents('php://input'), true);
+            $id = $data['id'];
+            $orderModel->deleteOrder($id);
+            http_response_code(200);
+
+            return;
+        } else {
+            http_response_code(400);
+            echo json_encode(array("error"=>"invalid endpoint"));
+        }
+    // TODO: Create get split order and get plan endpoint.
     default:
         http_response_code(405);
         echo json_encode(array("error"=>"invalid method " . $method));
