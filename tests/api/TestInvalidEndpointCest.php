3e2a3be3ebcc14233af17397314aa1fcc05bb2e5
Ruben Christoffer Hegland-Antonsen <rubench@stud.ntnu.no>
2021-05-21 13:16:30 +0200

    Fixed pipeline for API Testing

--- tests/api/TestInvalidEndpointCest.php
+++ tests/api/TestInvalidEndpointCest.php
@@ -1,12 +1,12 @@
 <?php
 
-class TestInvalidEndpointCest
-{
+class TestInvalidEndpointCest {
+
     public function _before(ApiTester $I) {}
 
-    // tests
-    public function tryToTest(ApiTester $I) {
-        $I->sendGet('dfgffdgd');
+    // Test that an endpoint that does not exist returns an error
+    public function testEndpointNotExisting (ApiTester $I) {
+        $I->sendGet('fdgdfg');
         $I->seeResponseContains('That endpoint does not exist');
     }
 
