704b8c9625197d341b768be414277c497edfb22a
Ruben Christoffer Hegland-Antonsen <rubench@stud.ntnu.no>
2021-05-23 13:37:18 +0200

    Fixed assert in CusRepEndpoint

--- tests/api/CusRepEndpointCest.php
+++ tests/api/CusRepEndpointCest.php
@@ -10,18 +10,7 @@ class CusRepEndpointCest {
         $I->haveHttpHeader('TOKEN', 'test-token');
         $I->sendGet("com/cus/orders");
         $I->seeResponseIsJson();
-        $I->seeResponseContains('{
-    "order_number": "1",
-    "total_price": "100",
-    "reference_to_larger_order": null,
-    "customer_id": "1",
-    "skis": [
-      {
-        "ski_type_id": "1",
-        "quantity": "4"
-      }
-    ]
-  },');
+        $I->seeResponseContains('[{"order_number":"1","total_price":"100","reference_to_larger_order":null,"customer_id":"1","skis":[{"ski_type_id":"1","quantity":"4"}]}');
     }
 
 }
