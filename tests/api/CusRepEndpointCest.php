1e01ea186a9fc0f5565ae428e7984caa9a1af41d
Ruben Christoffer Hegland-Antonsen <rubench@stud.ntnu.no>
2021-05-23 12:58:12 +0200

    Added CusRepEndpoint and testGETOrders

---
+++ tests/api/CusRepEndpointCest.php
@@ -0,0 +1,24 @@
+<?php
+
+class CusRepEndpointCest {
+
+    public function _before(ApiTester $I) {}
+
+    public function testGETOrders(ApiTester $I) {
+        $I->sendGet("com/cus/orders");
+        $I->seeResponseIsJson();
+        $I->seeResponseContains('{
+    "order_number": "1",
+    "total_price": "100",
+    "reference_to_larger_order": null,
+    "customer_id": "1",
+    "skis": [
+      {
+        "ski_type_id": "1",
+        "quantity": "4"
+      }
+    ]
+  },');
+    }
+
+}
